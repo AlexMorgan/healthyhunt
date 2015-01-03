79ccbee75c323bc6dc72f51a4566e05f1366f7a7
AlexMorgan <alexmorgan.am@gmail.com>
2015-01-03 17:41:44 -0500

    Finished testing for adding and deleting comments

--- spec/features/user_leaves_a_comment_on_posts_spec.rb
+++ spec/features/user_leaves_a_comment_on_posts_spec.rb
@@ -32,4 +32,22 @@ feature "User comments on a pose", %q{
     expect(page).to have_content "Please make sure your submission is complete"
   end
 
+  scenario "User deletes a comment" do
+    post = FactoryGirl.create(:post)
+    user = FactoryGirl.create(:user)
+    log_in(user)
+
+    visit root_path
+
+    visit post_path(post)
+    expect(page).to have_content "Leave Comment"
+
+    fill_in 'comment_body', with: 'This is a test'
+    click_on "Comment"
+    expect(page).to have_content "Your comment has been submitted"
+
+    click_on "Delete"
+    expect(page).to have_content "Your comment has been deleted"
+  end
+
 end
