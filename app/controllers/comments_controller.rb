b6f59b43e656a5cf302c84e8743b97b496d3ccbb
AlexMorgan <alexmorgan.am@gmail.com>
2015-01-03 00:48:20 -0500

    Started on comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
   def create
-
+    @comment = Comment.new
   end
 
   def destroy
+    @comm
+  end
+
+  private
+
+  def post_params
+    params.require(:comment).permit(:body).merge(user: current_user)
   end
 end
