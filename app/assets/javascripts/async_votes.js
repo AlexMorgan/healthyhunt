ee4ff5720b2b95c865c565623cc3b9d2484eab54
AlexMorgan <alexmorgan.am@gmail.com>
2015-01-05 15:12:15 -0500

    Finished asynchronous calls for creating and deleting upvotes

--- app/assets/javascripts/async_votes.js
+++ app/assets/javascripts/async_votes.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('[data-method="post"]').on('click', function(e) {
+  $('[data-post-id]').on('click', '[data-method="post"]', function(e) {
     e.stopPropagation();
     e.preventDefault();
     // Capture the target of the event
@@ -17,14 +17,10 @@ $(function(){
         // Create the String version of the form action
         // This needs to be fixed, otherwise it could potentially mess with all anchor tages on the page.
         // Create more posts and make sure
-        $downvoteLink = $('<a>').attr({
+        $voteLink = $('<a>').attr({
           'class': 'vote-down',
           'data-method': 'delete',
           'href': $url + '/' + upvote.id,
-        });
-
-        $newButton = $('<div>').attr({
-          'class': 'vote col-sm-2'
         });
 
         createUpvoteBtn(upvoteCount);
@@ -32,16 +28,39 @@ $(function(){
     });
   }); // End of data-method="post"
 
-  // $('data-post-id').on('click', function(e) {
 
-  // });
+  $('[data-post-id]').on('click', '[data-method="delete"]', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    // Capture the target of the event
+    $target = $(e.currentTarget);
+    // Get the parent of the target which holds the data-href attribute
+    $url = $target.attr('href');
+    $.ajax({
+      type: "DELETE",
+      url: $url,
+      dataType: "json",
+      success: function(data) {
+        upvoteCount = data.count
+
+        $voteLink = $('<a>').attr({
+          'class': 'vote-up',
+          'data-method': 'post',
+          'href': '/posts/' + data.post.id + '/upvotes',
+        });
+
+        createUpvoteBtn(upvoteCount);
+      }
+    });
+  });
 
   function createUpvoteBtn(upvoteCount) {
+    $newButton = $('<div>').attr('class', 'vote col-sm-2');
     $newSpan = $('<span>').append(upvoteCount);
     $upvoteChar = $('<span>').attr('class', 'block').append('&#9650;');
     $newButton.append($upvoteChar);
     $newButton.append($newSpan);
-    $downvoteLink.append($newButton);
-    $target.replaceWith($downvoteLink);
+    $voteLink.append($newButton);
+    $target.replaceWith($voteLink);
   }
 });
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -20,8 +20,18 @@ class UpvotesController < ApplicationController
     post = Post.find(params[:post_id])
     current_user.upvotes.destroy(params[:id])
 
-    flash[:notice] = "All evidence of your upvote has been destroyed!"
-    redirect_to :back
+    respond_to do |format|
+      # Respond the same way we were before if the request format is html
+      format.html do
+        flash[:notice] = "All evidence of your upvote has been destroyed!"
+        redirect_to :back
+      end
+
+      # Respond with a "204 No Content" to signify that the request has been
+      # fulfilled
+      upvote_count = post.upvotes.count
+      format.json { render json: {post: post, count: upvote_count} }
+    end
   end
 end
 
